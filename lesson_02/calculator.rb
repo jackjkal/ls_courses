3a1b60641313537bfd7b460a8e0601c0e9ace844
Jack Kalayjian <jackjkal@protonmail.com>
2023-07-15 23:26:07 -0700

    Correct Rubocop offenses

--- lesson_02/calculator.rb
+++ lesson_02/calculator.rb
@@ -84,14 +84,14 @@ loop do # main loop
 
   result = case operator
            when '1'
-            number1.to_i + number2.to_i
+             number1.to_i + number2.to_i
            when '2'
-            number1.to_i - number2.to_i
+             number1.to_i - number2.to_i
            when '3'
-            number1.to_i * number2.to_i
+             number1.to_i * number2.to_i
            when '4'
-            number1.to_f / number2.to_f
-  end
+             number1.to_f / number2.to_f
+           end
 
   prompt("The result is #{result}")
 
