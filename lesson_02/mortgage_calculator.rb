92139f932540e236bc74f37b35a4411e6b957d36
Jack Kalayjian <jackjkal@protonmail.com>
2023-07-30 11:00:48 -0700

    Add mortgage calculator boilerplate

---
+++ lesson_02/mortgage_calculator.rb
@@ -0,0 +1,17 @@
+require 'yaml'
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def number?(input)
+  integer?(input) || float?(input)
+end
