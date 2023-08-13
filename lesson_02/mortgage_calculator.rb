f151bb6b85b4a2a2858e527f49396a0c0811341b
Jack Kalayjian <jackjkal@protonmail.com>
2023-08-13 09:59:10 -0700

    Read and validate user input

--- lesson_02/mortgage_calculator.rb
+++ lesson_02/mortgage_calculator.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 
+MESSAGES = YAML.load_file('mortgage_messages.yml', symbolize_names: true)
+
 def prompt(message)
   puts "=> #{message}"
 end
@@ -14,4 +16,53 @@ end
 
 def number?(input)
   integer?(input) || float?(input)
+end
+
+prompt(MESSAGES[:welcome])
+
+loop do
+  loan_amount = ''
+  annual_percentage_rate = ''
+  loan_duration = ''
+
+  loop do
+    prompt(MESSAGES[:loan_amount])
+    loan_amount = gets.chomp
+
+    if number?(loan_amount)
+      loan_amount = loan_amount.to_i
+      break
+    else
+      prompt(MESSAGES[:valid_number])
+    end
+  end
+
+  loop do
+    prompt(MESSAGES[:apr])
+    annual_percentage_rate = gets.chomp
+
+    if integer?(annual_percentage_rate)
+      annual_percentage_rate = annual_percentage_rate.to_i
+      break
+    else
+      prompt(MESSAGES[:valid_integer])
+    end
+  end
+
+  loop do
+    prompt(MESSAGES[:loan_duration])
+    loan_duration = gets.chomp
+
+    if integer?(loan_duration)
+      loan_duration = loan_duration.to_i
+      break
+    else
+      prompt(MESSAGES[:valid_integer])
+    end
+  end
+
+  prompt(MESSAGES[:repeat])
+  answer = gets.chomp
+
+  break unless answer.downcase.start_with?('y')
 end
