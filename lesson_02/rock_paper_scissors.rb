f0895eb79eca0f5b90f94a757ebd502c2980faa7
Jack Kalayjian <jackjkal@protonmail.com>
2023-09-02 13:38:49 -0700

    Modify to include and validate lizard and spock options, including shorthand

--- lesson_02/rock_paper_scissors.rb
+++ lesson_02/rock_paper_scissors.rb
@@ -1,13 +1,43 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = {
+  words: ['rock', 'paper', 'scissors', 'lizard', 'spock'],
+  shorthand: {
+    'r' => 0,
+    'p' => 1,
+    's' => 2,
+    'l' => 3,
+    'sp' => 4
+  }
+}
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def valid_choice?(choice)
+  VALID_CHOICES[:words].include?(choice) ||
+  VALID_CHOICES[:shorthand].include?(choice)
+end
+
+def word?(text)
+  VALID_CHOICES[:words].include?(text)
+end
+
+def to_word(shorthand)
+  index = VALID_CHOICES[:shorthand][shorthand]
+
+  VALID_CHOICES[:words][index]
+end
+
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  win_conditions = {
+    'rock' => ['lizard', 'scissors'],
+    'paper' => ['rock', 'spock'],
+    'scissors' => ['paper', 'lizard'],
+    'lizard' => ['paper', 'spock'],
+    'spock' => ['rock', 'scissors']
+  }
+
+  win_conditions[first].include?(second)
 end
 
 def display_result(player, computer)
@@ -23,17 +53,33 @@ end
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    prompt("Choose one: #{VALID_CHOICES[:words].join(', ')}")
 
-    if VALID_CHOICES.include?(choice)
+    shorthand_options = <<-MSG
+    r  = rock
+    p  = paper
+    s  = scissors
+    l  = lizard
+    sp = spock
+    MSG
+
+    prompt("You can also use shorthand:")
+    Kernel.puts(shorthand_options)
+
+    choice = Kernel.gets().chomp().downcase()
+
+    if valid_choice?(choice)
+      unless word?(choice)
+        choice = to_word(choice)
+      end
+      
       break
     else
       prompt("That's not a valid choice")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES[:words].sample
 
   Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
 
